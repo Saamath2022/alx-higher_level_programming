55395a46b12d217fdd8d3a660eb7d957023a8677
Saamath2023 <saaaondow@gmail.com>
2024-03-15 21:02:20 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,10 +6,10 @@ from calculator_1 import add, subtract, multiply, divide
 
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+        exit(1)
 
     a = int(sys.argv[1])
-    b = int(sys.argv[2])
+    b = int(sys.argv[3])
 
     operator = sys.arg[2]
     if operator == "+":
@@ -19,7 +19,7 @@ from calculator_1 import add, subtract, multiply, divide
     elif operator == "*":
         print("{} * {} = {}".format(a, b, add(a, b)))
      elif operator == "/":
-        print("{} / {}= {}".format(a, b, add(a, b)))
+        print("{} / {} = {}".format(a, b, add(a, b)))
     else:
         print("Unknown operator. Availabe operators: +, -, * and /")
         exit(1)
