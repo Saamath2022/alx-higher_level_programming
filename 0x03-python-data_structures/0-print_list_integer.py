335394d6fd334ffff827b45ce6b8733d4736c86a
Saamath2023 <saaaondow@gmail.com>
2024-03-17 05:07:24 -0700

    task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    """
-    Prints all integers of a list.
-
-    Prototype: def print_list_integer(my_list=[]):
-    Format: one integer per line. See example
-    You are not allowed to import any module
-    You can assume that the list only contains integers
-    You are not allowed to cast integers into strings
-    You have to use str.format() to print integers
-    """
-    for i in my_list:
+    for num in my_list:
         print("{:d}".format(num))
