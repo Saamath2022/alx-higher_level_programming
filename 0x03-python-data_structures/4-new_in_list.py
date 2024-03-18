9f778df74f51f0e7cf7ffab58f775b7d49bffa12
Saamath2023 <saaaondow@gmail.com>
2024-03-18 13:04:35 -0700

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,8 @@ def get_element_or_copy(my_list=[], idx=0):
     if idx < 0:
         return my_list.copy()
     elif idx < len(my_list):
-        return my_list[idx]
+        modified_list = my_list.copy()
+        modified_list[idx] = 9
+        return modified_list
     else:
         return None
