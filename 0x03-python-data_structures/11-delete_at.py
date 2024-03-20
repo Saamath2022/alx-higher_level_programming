6ee484a109f5a5d9744472eda0f867464f87803a
Saamath2023 <saaaondow@gmail.com>
2024-03-19 19:38:58 -0700

    11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    result = []
+delete_at = __import__('11-delete_at').delete_at
 
-    for i in range(len(my_list)):
-        if i != indx:
-            result.append(my_list[i])
-        return result
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
