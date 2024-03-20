bbad21c96d15b1924f5256d8e07cdd9952740986
Saamath2023 <saaaondow@gmail.com>
2024-03-19 19:18:19 -0700

    10-divisible_by_2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     result = []
-    for i, num in enumerate(my_list):
-        if num % == 0:
+    for num in my_list:
+        if num & 2 == 0:
             result.append(True)
-            print(f"{y} is divisble by 2")
         else:
             result.append(False)
-            print(f"{y} is not divisible by 2")
-    return result
+        return result
