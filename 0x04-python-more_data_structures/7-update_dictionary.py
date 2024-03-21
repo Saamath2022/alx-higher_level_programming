1509a3fad2095e7899e01b9e7b496f5fe2e62d8e
Saamath2023 <saaaondow@gmail.com>
2024-03-20 21:21:10 -0700

    7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
-    for k, v in sorted(a_dictionary.items()):
-        print("{}: {}".format(k, v))
-    print("--")
