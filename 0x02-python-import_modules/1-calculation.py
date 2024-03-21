bb311e00f8cb452192c2d7205476c90ff6a13f2e
Saamath2023 <saaaondow@gmail.com>
2024-03-20 18:55:41 -0700

    1-calculation.py

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    operations = [
+        (add, "+"),
+        (sub, "-"),
+        (mul, "*"),
+        (div, "/")
+    ]
+
+    for operation, symbol in operations:
+        result = operation(a, b)
+        print("{} {} {} = {}".format(a, symbol, b, result))
