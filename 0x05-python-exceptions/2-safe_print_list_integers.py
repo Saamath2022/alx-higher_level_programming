1c8120a3480c8a674334a95da76193dd8d5193f1
Saamath2023 <saaaondow@gmail.com>
2024-04-03 17:50:23 -0700

    2-save_print_list_intergers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try
+    t_print = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end='')
+        t_print += 1
+        except IndexError:
+            break
+    print("")
+    return (t_print)
