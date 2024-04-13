2420a65b6a34059c9938dee4d6328d2c973afcc7
Saamath2023 <saaaondow@gmail.com>
2024-04-12 21:43:11 -0700

    5-text_indentation.py

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,22 +1,79 @@
 #!/usr/bin/python3
-"""Define a matrix multiplication function"""
+"""Module for matrix_mul method."""
 
 
 def matrix_mul(m_a, m_b):
-    """Multiply two matrices
-
+    """Multiplies one matrix by another.
     Args:
-        m_a (list of lists of int/floats): The first matrix.
-        m_b (lists of lists of instance floats) the second matrix.
-
-    Raises:
-        TypeError: if either m_a or m_b is empty, or if they are not lists.
-        TypeError: if either m_a or m_b has different sized rows.
-        ValueError: if m_a and m_b cannot be multiplied.
-
+        m_a: the first matrix
+        m_b: the second matrix
     Returns:
-        A new matrix representing the multiplication of m_a by m_b
+        matrix: the product
+    Raises:
+        TypeError: If m_a or m_b are not lists.
+        TypeError: If m_a or m_b are not lists of lists.
+        ValueError: If m_a or m_b are empty lists/matrices.
+        TypeError: If m_a or m_b contain a non int/float.
+        TypeError: If m_a or m_b are not rectangular.
+        ValueError: If m_a or m_b can't be multiplied.
     """
 
-    if not`:wq
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+    m_a_empty = False
+    m_b_empty = False
+    m_a_notrect = False
+    m_b_notrect = False
+    m_a_notnum = False
+    m_b_notnum = False
+    for row in m_a:
+        if not isinstance(row, list):
+            raise TypeError("m_a must be a list of lists")
+        if len(row) != len(m_a[0]):
+            m_a_notrect = True
+        for num in row:
+            if not isinstance(num, (int, float)):
+                m_a_notnum = True
 
+    for row in m_b:
+        if not isinstance(row, list):
+            raise TypeError("m_b must be a list of lists")
+        if len(row) != len(m_b[0]):
+            m_b_notrect = True
+        for num in row:
+            if not isinstance(num, (int, float)):
+                m_b_notnum = True
+
+    if len(m_a) == 0 or (len(m_a) == 1 and len(m_a[0]) == 0):
+        raise ValueError("m_a can't be empty")
+
+    if len(m_b) == 0 or (len(m_b) == 1 and len(m_b[0]) == 0):
+        raise ValueError("m_b can't be empty")
+
+    if m_a_notnum:
+        raise TypeError("m_a should contain only integers or floats")
+
+    if m_b_notnum:
+        raise TypeError("m_b should contain only integers or floats")
+
+    if m_a_notrect:
+        raise TypeError("each row of m_a must should be of the same size")
+
+    if m_b_notrect:
+        raise TypeError("each row of m_b must should be of the same size")
+
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    mos = [[] for i in range(len(m_a))]
+
+    for i in range(len(m_a)):
+        for j in range(len(m_b[0])):
+            c = 0
+            for k in range(len(m_b)):
+                c += m_a[i][k] * m_b[k][j]
+            res[i].append(c)
+
+    return mos
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,12 +1,29 @@
-def text_indentation(text):
+#!/usr/bin/python3
+"""Module for text_indentation method."""
 
+
+def text_indentation(text):
+    """Method for adding 2 new lines after '.?:' chars.
+
+    Args:
+        text: The str text.
+
+    Raises:
+        TypeError: If text is not a str.
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    new_line_chars = ['.', '?', ':']
+    for delim in ".?:":
+        # Split text by delimiter
+        parts = text.split(delim)
+        # Join parts with delimiter and two new lines
+        text = (delim + "\n\n").join(parts)
+    print(text, end="")
 
-    for char in text:
-        print(char, end='')
 
-        if char in new_line_chars:
-            print("\n" * 2, end='')
+if __name__ == "__main__":
+    """Importing the doctest module"""
+    import doctest
+    """ Running the test cases from the specified file"""
+    doctest.testfile("tests/5-text_indentation.txt")
