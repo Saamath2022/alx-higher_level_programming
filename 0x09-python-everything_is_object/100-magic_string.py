cd7e180e6e6fafb8e30d280239d22e548c3eac63
Saamath2023 <saaaondow@gmail.com>
2024-04-21 12:34:10 -0700

    100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    """
-    Returns a string "BestSchool" concatenated n times,
-    where n is the number of the iteration.
-    """
-    return "BestSchool" * magic_string.counter
-
-
-magic_string.counter = 1
+    magic_string.count = getattr(magic_string, 'count', 0) + 1
+    return ", ".join(["BestSchool" for i in range(magic_string.count)])
