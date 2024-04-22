a72026dbd5af662af6b73e06951832a089da956a
Saamath2023 <saaaondow@gmail.com>
2024-04-22 09:23:16 -0700

    models/rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -53,3 +53,13 @@ class Rectangle(Base):
     def y(self, value):
         """Setter for the y attribute."""
         self.__y = value
+
+    def validate_integer(self, name, value, eq=True):
+    """Method for validating the value"""
+    if not isinstance(value, int):
+        raise TypeError("{} must be an integer".format(name))
+    if eq and value < 0:
+        raise ValueError("{} must be >= 0".format(name))
+    elif not eq and value <= 0:
+        raise ValueError("{} must be > 0".format(name))
+
