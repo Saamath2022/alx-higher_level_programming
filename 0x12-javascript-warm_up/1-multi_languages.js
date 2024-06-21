b1d94391d656eed1ab1a0a300be61e3f4ec99ab0
saamath2022 <saaaondow@mail.com>
2024-06-21 16:52:24 -0700

    1-multi_language.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -5,7 +5,7 @@ const myMultiple = {
 	myString2: "Python is cool",
 	myString3: "JavaScript is amazing",
 	myMethod: function() {
-		return `${this.myString1}\nC is fun\n${this.myString3}.`;
+		return `${this.myString1}\nPython is cool\n${this.myString3}.`;
 	}
 };
 console.log(myMultiple.myMethod());
