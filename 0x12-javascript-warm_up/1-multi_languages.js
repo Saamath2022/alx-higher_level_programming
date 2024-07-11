9d5901f0d52c8ed2224864100b2e998dc02dd37a
saamath2022 <saaaondow@mail.com>
2024-07-11 12:34:33 -0700

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
-const myMultiple = {
+const myStrings = {
 	myString1: "C is fun",
 	myString2: "Python is cool",
-	myString3: "JavaScript is amazing",
-	myMethod: function() {
-		return `${this.myString1}\nPython is cool\n${this.myString3}.`;
-	}
+	myString3: "JavaScript is amazing"
 };
-console.log(myMultiple.myMethod());
+
+console.log(myStrings.myString1);
+console.log(myStrings.myString2);
+console.log(myStrings.myString3);
+
---
+++ 0x12-javascript-warm_up/modules.js
@@ -0,0 +1,7 @@
+// module.js
+export const myVar = "value";
+
+// main.js
+import { myVar } from './module.js';
+console.log(myVar);
+
